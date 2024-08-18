eeab27768574cf8d5acc03e0aa432737b9edf6f8
oscar.elsasser <oscar.elsasser@saberastro.com>
2024-08-18 17:01:08 +1000

    directories don't reference dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/js/generate_playlist.js',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, ''),
     },
 }
         
